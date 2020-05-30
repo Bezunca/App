aa114ee1bb8cb904f730489e913d02527201cf4c
Guilherme Freire <guilhermebfreire@gmail.com>
2020-05-30 05:14:00 +0100

    Adedd different types of investments

--- lib/asset.dart
+++ lib/asset.dart
@@ -1,12 +1,22 @@
+import 'package:flutter/material.dart';
+
 class Asset {
   final String _fantasyName;
   final String _ticker;
   final String currency;
+  final String _type;
   int _price; // Multiplied by 100
 
-  Asset(this._fantasyName, this._ticker, this._price, {this.currency="R\$"});
+  final Map<String, IconData> _assetTypeIcons= {
+    "share": Icons.import_contacts,
+    "reit": Icons.home,
+    "etf": Icons.equalizer,
+  };
+
+  Asset(this._fantasyName, this._ticker, this._price, this._type, {this.currency="R\$"});
 
   String get price => "$currency${(_price/100.0).toStringAsFixed(2)}";
   String get ticker => _ticker;
   String get name => _fantasyName;
+  IconData get icon => _assetTypeIcons[this._type];
 }
--- lib/main.dart
+++ lib/main.dart
@@ -15,11 +15,17 @@ class MyApp extends StatelessWidget {
 
 class PortfolioState extends State<Portfolio> {
   final _portfolio = <Asset>[
-    Asset("Itaúsa", "ITSA4", 886),
-    Asset("Fleury", "FLRY3", 2335),
-    Asset("Energias do Brasil", "ENBR3", 1767),
-    Asset("Sinqia", "SQIA3", 1961),
-    Asset("B3", "B3SA3", 4530),
+    Asset("Itaúsa", "ITSA4", 886, "share"),
+    Asset("Fleury", "FLRY3", 2335, "share"),
+    Asset("Energias do Brasil", "ENBR3", 1767, "share"),
+    Asset("Sinqia", "SQIA3", 1961, "share"),
+    Asset("B3", "B3SA3", 4530, "share"),
+    Asset("CSHG LOGÍSTICA FDO INV IMOB", "HGLG11", 18195, "reit"),
+    Asset("XP LOG FDO INV IMOB", "XPLG11", 11500, "reit"),
+    Asset("HSI MALL FDO INV IMOB", "HSML11", 8320, "reit"),
+    Asset("KINEA RENDA IMOBILIÁRIA FDO INV IMOB", "KNRI11", 17239, "reit"),
+    Asset("CSHG RENDA URBANA", "HGRU11", 11597, "reit"),
+    Asset("IShare SP500CI", "IVVB11", 11597, "etf")
   ];
 
   // Fonts
@@ -62,7 +68,7 @@ class PortfolioState extends State<Portfolio> {
           asset.ticker,
           style: _smallerFont,
         ),
-        leading: Icon(Icons.equalizer),
+        leading: Icon(asset.icon),
         trailing: Text(
           asset.price,
           style: _normalFont,
